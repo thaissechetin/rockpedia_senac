ade6b3c48668a716897632dfdbcaa18a82816262
Izabelle Santos do Nascimento <izabelle.sdn@gmail.com>
2022-06-23 14:44:58 -0300

    cards e slick

--- includes/outras-materias.php
+++ includes/outras-materias.php
@@ -2,16 +2,14 @@
 require "funcoes-posts.php";
 $posts = lerTodosOsPosts($conexao);
 ?>
-<div class="row my-1 mx-md-n1">
+<div id="slider">
     <?php foreach ($posts as $post) { ?>
-        <div class="col-sm-12 col-md-6 col-lg-3 my-1 px-md-1">
-            <div class="card">
-                <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
-                <div class="card-body d-flex flex-column">
-                    <h5 class="card-title"><?= $post['titulo'] ?></h5>
-                    <p class="card-text"><?= $post['descricao'] ?></p>
-                    <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="btn btn-dark mt-auto">Ler mais!</a>
-                </div>
+        <div class="card">
+            <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title"><?= $post['titulo'] ?></h5>
+                <p class="card-text"><?= $post['descricao'] ?></p>
+                <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="btn btn-dark mt-auto">Ler mais!</a>
             </div>
         </div>
     <?php } ?>
