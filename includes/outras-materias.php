65bc65d9965af8a05d5094d8d9a339e584edddcb
Thaís Sechetin <93165270+thaissechetin@users.noreply.github.com>
2022-06-23 14:28:59 -0300

    clip-path

--- includes/outras-materias.php
+++ includes/outras-materias.php
@@ -5,7 +5,7 @@ $posts = lerTodosOsPosts($conexao);
 <div class="row my-1 mx-md-n1">
     <?php foreach ($posts as $post) { ?>
         <div class="col-sm-12 col-md-6 col-lg-3 my-1 px-md-1">
-            <div class="card">
+            <div class="card-header">
                 <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title"><?= $post['titulo'] ?></h5>
