ad1e9e43f604eccfb9117412160b2d6b13af3d67
Izaart <izabelle.sdn@gmail.com>
2022-06-26 14:57:37 -0300

    card-link bandas.php

--- bandas.php
+++ bandas.php
@@ -7,15 +7,16 @@ $posts = lerTodosOsPosts($conexao);
     <div class="row my-1 mx-md-n1">
         <?php foreach ($posts as $post) { ?>
             <div class="col-md-6 my-1 px-md-1">
-                <div class="card h-100 border border-danger">
-                    <span class="mask-imagem">
-                        <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
-                    </span>
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title"><?= $post['titulo'] ?></h5>
-                        <p class="card-text"><?= $post['descricao'] ?></p>
-                        <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="btn btn-dark mt-auto">Ler mais!</a>
-                    </div>
+                <div class="card border border-danger h-100">
+                    <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="card-link">
+                        <span class="mask-img">
+                            <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
+                        </span>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $post['titulo'] ?></h5>
+                            <p class="card-text"><?= $post['descricao'] ?></p>
+                        </div>
+                    </a>
                 </div>
             </div>
         <?php } ?>
