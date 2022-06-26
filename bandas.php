2cfef05155f3949c6936b193fc683fdfc8a32577
Izaart <izabelle.sdn@gmail.com>
2022-06-26 15:01:03 -0300

    correção card-link bandas.php

--- bandas.php
+++ bandas.php
@@ -7,17 +7,18 @@ $posts = lerTodosOsPosts($conexao);
     <div class="row my-1 mx-md-n1">
         <?php foreach ($posts as $post) { ?>
             <div class="col-md-6 my-1 px-md-1">
-                <div class="card border border-danger h-100">
-                    <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="card-link">
-                        <span class="mask-img">
+                <a href="post-detalhe.php?id=<?= $post['id'] ?>" class="card-link">
+                    <div class="card h-100 border border-danger">
+                        <span class="mask-imagem">
                             <img class="card-img-top" src="imagens/<?= $post['imagem'] ?>" alt="Imagem de destaque do post">
                         </span>
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title"><?= $post['titulo'] ?></h5>
                             <p class="card-text"><?= $post['descricao'] ?></p>
+                            <button class="btn btn-dark">Ler mais!</button>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
         <?php } ?>
     </div>
